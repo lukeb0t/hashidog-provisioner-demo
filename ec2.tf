6e8e0193add9cbb5fc9fac79e57b2f82fdc00f55
Luke McCleary <luke.mccleary@gmail.com>
2022-04-21 16:03:18 -0400

    subnet number

--- ec2.tf
+++ ec2.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "hashidog" {
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.hashidog.key_name
   associate_public_ip_address = true
-  subnet_id                   = module.vpc.public_subnets
+  subnet_id                   = module.vpc.public_subnets[0].id
   vpc_security_group_ids      = [aws_security_group.hashidog.id]
 
   connection {
