8ef4170759a6ee3f6090fd1985e0eb8020f7943d
Luke McCleary <luke.mccleary@gmail.com>
2022-04-21 17:07:31 -0400

    /16

--- networking.tf
+++ networking.tf
@@ -6,10 +6,10 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "hashidog-vpc"
-  cidr = "10.0.0.0/8"
+  cidr = "10.0.0.0/16"
 
   azs             = data.aws_availability_zones.azs.names
-  private_subnets = cidrsubnets("10.0.0.0/8", 4, 4, 4, 4, 4, 4, 4)
+  private_subnets = cidrsubnets("10.0.0.0/16", 4, 4, 4, 4, 4, 4, 4)
   public_subnets  = cidrsubnets("10.10.0.0/16", 4, 4, 4, 4, 4, 4, 4)
 
   enable_nat_gateway = true
