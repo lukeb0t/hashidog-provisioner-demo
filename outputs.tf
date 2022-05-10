a4740a578fb4cd40c6f32555d05c34b262625623
Luke McCleary <luke.mccleary@gmail.com>
2022-05-10 14:03:36 -0400

    awdawd

--- outputs.tf
+++ outputs.tf
@@ -34,11 +34,11 @@ output "keypair_pem" {
 }
 
 output "hashidog_public_ip" {
-  value = aws_instance.hashidog[count.index].public_ip
+  value = aws_instance.hashidog[*].public_ip
 }
 
 output "hashidog_private_ip" {
-  value = aws_instance.hashidog[count.index].private_ip
+  value = aws_instance.hashidog[*].private_ip
 }
 
 output "hashidog_sg_id" {
