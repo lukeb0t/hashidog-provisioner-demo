687b623c48ef44814afa72a10708cc55e79a03d4
Luke McCleary <luke.mccleary@gmail.com>
2022-05-10 12:15:15 -0400

    count = 2

--- ec2.tf
+++ ec2.tf
@@ -29,6 +29,7 @@ resource "random_shuffle" "public_subnets" {
 }
 
 resource "aws_instance" "hashidog" {
+  count = 2
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.hashidog.key_name
