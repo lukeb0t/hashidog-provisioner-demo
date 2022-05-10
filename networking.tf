2003782a5ddd37f7a6fb1411854dada95eb4a152
Luke McCleary <luke.mccleary@gmail.com>
2022-05-10 14:34:36 -0400

    updated SG

--- networking.tf
+++ networking.tf
@@ -20,25 +20,32 @@ module "vpc" {
 resource "aws_security_group" "hashidog" {
   name   = "hashidog"
   vpc_id = module.vpc.vpc_id
-  egress {
-    # allow all outbound
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   ingress {
-    # Allow inbound SSS from anywhere. Using bastion for provisioning. This should be disabled for production
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
-    # allowing gloabl access for UI demo and build. Do not do this in production
-    from_port   = 8200
-    to_port     = 8205
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
   }
 }
