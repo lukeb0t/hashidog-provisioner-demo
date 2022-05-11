a91043f12e6f2dd05cfa43467162f24b064c7550
Luke McCleary <luke.mccleary@gmail.com>
2022-05-11 12:02:52 -0400

    try3

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 sudo apt -y update
+sleep 10
+sudo apt -y update
 sudo apt -y install apache2
+sudo apt -y update && sudo apt -y upgrade
 sudo systemctl start apache2
 sudo chown -R ubuntu:ubuntu /var/www/html
 
