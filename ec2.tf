e34b836e2d5a307d593458dae32fdf1beb6cab08
Luke McCleary <luke.mccleary@gmail.com>
2022-05-11 09:07:07 -0400

    removed eip and provisioners

--- ec2.tf
+++ ec2.tf
@@ -28,17 +28,17 @@ resource "random_shuffle" "public_subnets" {
   result_count = 1
 }
 
-resource "aws_eip" "hashidog" {
-  count = var.num_instances
-  instance = aws_instance.hashidog[count.index].id
-  vpc      = true
-}
-
-resource "aws_eip_association" "argonet" {
-  count = var.num_instances
-  instance_id   = aws_instance.hashidog[count.index].id
-  allocation_id = aws_eip.hashidog[count.index].id
-}
+# resource "aws_eip" "hashidog" {
+#   count = var.num_instances
+#   instance = aws_instance.hashidog[count.index].id
+#   vpc      = true
+# }
+# 
+# resource "aws_eip_association" "argonet" {
+#   count = var.num_instances
+#   instance_id   = aws_instance.hashidog[count.index].id
+#   allocation_id = aws_eip.hashidog[count.index].id
+# }
 
 resource "aws_instance" "hashidog" {
   count = var.num_instances
@@ -49,32 +49,32 @@ resource "aws_instance" "hashidog" {
   subnet_id                   = random_shuffle.public_subnets.result[0]
   vpc_security_group_ids      = [aws_security_group.hashidog.id]
 
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    private_key = tls_private_key.hashidog.private_key_pem
-    host        =  self.public_ip
-  }
- 
-  provisioner "file" {
-    source      = "files/"
-    destination = "/home/ubuntu/"
-  }
-
-  provisioner "remote-exec" {
-    on_failure = continue
-    inline = [
-      "sudo apt -y update",
-      "sudo apt -y install apache2",
-      "sudo systemctl enable apache2",
-      "sudo systemctl start apache2",
-      "sudo chown -R ubuntu:ubuntu /var/www/html",
-      "chmod +x *.sh",
-      "ls /home/ubuntu/",
-      "PLACEHOLDER=${var.placeholder} WIDTH=${var.width} HEIGHT=${var.height} PREFIX=${var.prefix}",
-      "./deploy_app.sh",
-      #"sudo apt -y install cowsay",
-      #"cowsay -f tux 'Mooooooorrrrnin!'",
-    ]
-  }
+#  connection {
+#    type        = "ssh"
+#    user        = "ubuntu"
+#    private_key = tls_private_key.hashidog.private_key_pem
+#    host        =  self.public_ip
+#  }
+# 
+#  provisioner "file" {
+#    source      = "files/"
+#    destination = "/home/ubuntu/"
+#  }
+#
+#  provisioner "remote-exec" {
+#    on_failure = continue
+#    inline = [
+#      "sudo apt -y update",
+#      "sudo apt -y install apache2",
+#      "sudo systemctl enable apache2",
+#      "sudo systemctl start apache2",
+#      "sudo chown -R ubuntu:ubuntu /var/www/html",
+#      "chmod +x *.sh",
+#      "ls /home/ubuntu/",
+#      "PLACEHOLDER=${var.placeholder} WIDTH=${var.width} HEIGHT=${var.height} PREFIX=${var.prefix}",
+#      "./deploy_app.sh",
+#      "sudo apt -y install cowsay",
+#      "cowsay -f tux 'Mooooooorrrrnin!'"
+#    ]
+#  }
 }
