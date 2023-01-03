7a13398a4e4c3798cdae489b5e2b50d1fa841027
Luke McCleary <luke.mccleary@gmail.com>
2023-01-03 13:37:45 -0500

    versions

--- versions.tf
+++ versions.tf
@@ -1,10 +1,14 @@
 terraform {
   required_providers {
-      aws = {
+    aws = {
       source  = "hashicorp/aws"
     }
     random = {
       source = "hashicorp/random"
+    }
+    doormat = {
+      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
+      version = "~> 0.0.0"
     }
 }
 
