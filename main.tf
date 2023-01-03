c446e1410cea9b1e8d78dce0f976b80fbbd7f89b
Luke McCleary <luke.mccleary@gmail.com>
2023-01-03 13:35:45 -0500

    doormat updates

--- main.tf
+++ main.tf
@@ -2,14 +2,25 @@ provider "aws" {
   default_tags {
    tags = local.tags
 }
-region = var.region
+  region = var.region
+  access_key = data.doormat_aws_credentials.creds.access_key
+  secret_key = data.doormat_aws_credentials.creds.secret_key
+  token      = data.doormat_aws_credentials.creds.token
+}
+
+provider "doormat" {}
+
+data "doormat_aws_credentials" "creds" {
+  provider = doormat
+
+  role_arn = "arn:aws:iam::325038557378:role/argonet_provisioner"
 }
 
 locals {
   tags = {
     owner              = var.owner
     se-region          = "AMER-East E2",
-    purpose            = "Terraform Demo of AWS Ifra-as-code",
+    purpose            = "Terraform Demo using provisioners for customization",
     ttl                = var.ttl,
     terraform          = "true",
     hc-internet-facing = "true",
